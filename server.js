c39b4f403d162ceb7e1b205962ade48ab2d3daab
YeomeoR <76974577+YeomeoR@users.noreply.github.com>
2021-02-10 20:09:59 +0000

    day1

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+// const logger = require('./middleware/logger')
+const morgan = require('morgan')
+
+//router files
+const bootcamps = require('./routes/bootcamps')
+
+//load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// app.use(logger)
+// dev logging middleware from morgan instead of the above homemade logger
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+  }
+
+// mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+);
