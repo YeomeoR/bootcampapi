32e1fff6a6979b4ab4f36ca4f865c43d5b6ec831
YeomeoR <76974577+YeomeoR@users.noreply.github.com>
2021-02-11 20:00:49 +0000

    Day 2
    Mongoose, MongoDB Cluster and Compass, Colors for the terminal, Postman optimisation and started CRUD functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,31 +1,49 @@
+// bring in the model
+const Bootcamp = require('../models/Bootcamp');
+const models = require('../models/Bootcamp');
+
 // @desc    GET all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'})
-}
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+};
 
 // @desc    GET single bootcamp
 // @route   GET /api/v1/bootcamp/:id
 // @access  Public
 exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`})
-}
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+};
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Create new bootcamp'})
-}
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  try {
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamp/:id
 // @access  Private
 exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`})
-}
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
 // @desc    Delete bootcamp
 // @route   DELETE /api/v1/bootcamp/:id
 // @access  Private
 exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`})
-}
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger')
 const morgan = require('morgan')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' });
+
+// connect ot db
+connectDB();
 
 //router files
 const bootcamps = require('./routes/bootcamps')
 
 //load env vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//body parser
+app.use(express.json());
 
 // app.use(logger)
 // dev logging middleware from morgan instead of the above homemade logger
@@ -26,7 +35,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline.bold),
 );
+
+// handle unhandled promise rejections
+process.on('unhandled rejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // close server, exit the process and crash the app
+  server.close(() => process.exit(1))
+})
